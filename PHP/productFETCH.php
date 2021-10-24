57f67cb5da95b0153e13aaddc2b22628f2496ced
glauciapamponet <glauciapamponet@gmail.com>
2021-10-23 22:25:56 -0300

    Página de Produto

--- PHP/productFETCH.php
+++ PHP/productFETCH.php
@@ -5,14 +5,23 @@
     $columns = array('prod.idproduto', 'prod.nomeprod', 'prod.precoprod', 'cat.nomecat', 'pt.nomepetiano', 'prod.pathimage',
     'prod.status_dispo');
 
+    $trans = array("ç" => "c", "ã" => "a", "á" => "a", "ó" => "o", "ô" => "o");
+
     $queryCATEG = "SELECT nomecat FROM categoria";
     $result = mysqli_query($connect, $queryCATEG);
 
     $data = '<div class="nav-main"> <ul class="nav nav-tabs" id="myTab" role="tablist">';
 
+    $cont = 0;
+
     while($rowTAB = mysqli_fetch_array($result)){
-      $data .='<li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#'.$rowTAB["nomecat"].'"
+      $estringue = '';
+      if($cont == 0){
+        $estringue .= ' active';
+      }
+      $data .='<li class="nav-item"><a class="nav-link '.$estringue.'" data-toggle="tab" href="#'.$rowTAB["nomecat"].'"
               role="tab">'.$rowTAB["nomecat"].'</a></li>';
+      $cont++;
     }
     $data .= '</ul></div> <div class="tab-content" id="myTabContent">';
 
@@ -39,26 +48,17 @@
         '<div class="col-xl-3 col-lg-4 col-md-4 col-12">
           <div class="single-product">
             <div class="product-img">
-              <a href="product-details.html">
+              <a href="singleshop.php?idproduto='.$rowPRODS["idproduto"].'">
                 <img class="default-img" src="'.$rowPRODS["pathimage"].'" alt="#">
                 <img class="hover-img" src="'.$rowPRODS["pathimage"].'" alt="#">
               </a>
               <div class="button-head">
-                <div class="product-action">
-                  <a data-toggle="modal" data-target="#exampleModal"
-                    title="Quick View" href="#"><i class=" ti-eye"></i><span>Quick Shop</span></a>
-                  <a title="Wishlist" href="#"><i
-                      class=" ti-heart "></i><span>Add to
-                      Wishlist</span></a>
-                  <a title="Compare" href="#"><i
-                      class="ti-bar-chart-alt"></i><span>Add to
-                      Compare</span></a>
-                </div>
+
                 <div class="product-action-2"><a title="Add to cart" href="#">Add to cart</a></div>
               </div>
             </div>
             <div class="product-content">
-              <h3><a href="product-details.html">'.$rowPRODS["nomeprod"].'</a></h3>
+              <h3><a href="singleshop.php?idproduto='.$rowPRODS["idproduto"].'">'.$rowPRODS["nomeprod"].'</a></h3>
               <div class="product-price"> <span>R$'.$rowPRODS["precoprod"].'</span> </div>
             </div>
           </div>
