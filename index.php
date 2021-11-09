f785e5507d72fa6095b268d1547ccb36848e8639
Alexandre Kenji Okamoto <49002667+alexandrekenji19@users.noreply.github.com>
2021-11-08 23:22:52 -0300

    Teste

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<!-- Title Tag  -->
-	<title>ShoPET</title>
+	<title>ShoPET Testando</title>
 	<!-- Favicon -->
 	<link rel="icon" type="image/png" href="images/LogoIcone.png">
 	<!-- Web Font -->
